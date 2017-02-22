f60a5680709c3e952ade23ec56d198487b464e36
Isabel Lee <ijl0322@gmail.com>
2017-02-22 13:56:15 -0600

    share on twitter ok

--- Photo Phabulous/Photo Phabulous/ImageViewController.swift
+++ Photo Phabulous/Photo Phabulous/ImageViewController.swift
@@ -6,13 +6,29 @@
 //  Copyright © 2017 Isabel  Lee. All rights reserved.
 //
 
+// - Attribution: https://www.youtube.com/watch?v=a89c67TIh10
+// - Attribution: https://www.youtube.com/watch?v=dfy7tu5m-p8
+// - Attribution: https://www.youtube.com/watch?v=a89c67TIh10
+
+
 import UIKit
+import Social
 
 class ImageViewController: UIViewController {
     
     var image: UIImage?
     
     @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func shareButton(_ sender: UIBarButtonItem) {
+        if SLComposeViewController.isAvailable(forServiceType: SLServiceTypeTwitter){
+            let vc = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+            vc?.add(image)
+            present(vc!, animated: true, completion: nil)
+        } else {
+            print("Service not available")
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
